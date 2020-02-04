fa1858abec7aad07403ced3e4123f95b554d31dc
Philiplewis35 <philiplewis35@gmail.com>
2020-02-04 13:54:19 +0000

    start ignore function

--- parser.rb
+++ parser.rb
@@ -7,7 +7,6 @@ require 'sinatra'
 require 'sinatra/cors'
 require 'pry'
 require 'json'
-# require 'awesome_print'
 
 set :allow_origin, '*'
 set :allow_methods, "POST"
@@ -15,6 +14,7 @@ set :allow_headers, "Content-Length, Content-Type, X-Content-Type-Options"
 set :expose_headers, "Content-Length, Content-Type, X-Content-Type-Options"
 
 $index = 0
+$ignored_text = []
 
 def passive_voice(text)
   passive_sentences = passive_sentences(text)
@@ -30,7 +30,7 @@ def passive_sentences(text)
   sentences = PragmaticSegmenter::Segmenter.new(text: text).segment
   passive_sentences = []
   sentences.map do |sentence|
-    passive_sentences << sentence if passive_sentence?(sentence) && !passive_exception?(sentence)
+    passive_sentences << sentence if !($ignored_text.include? sentence) && passive_sentence?(sentence) && !passive_exception?(sentence)
   end
   passive_sentences
 end
@@ -49,18 +49,20 @@ def convert_to_active_voice(passive_sentence)
 end
 
 def format_text(text)
-  # binding.pry
   text = text.force_encoding('UTF-8')
   text.gsub!("\xE2\x80\x8C", '')
   text.gsub!("\u00A0", ' ')
-  puts text
   text
 end
 
 post '/' do
   text = format_text(request.body.string)
-  # puts "text: " + text
   response.header.update({"Content-Type" => 'text/json', "X-Content-Type-Options" => 'nosniff'})
-  puts passive_voice(text).to_json
   passive_voice(text).to_json
+end
+
+post '/ignore' do
+  puts 'ignore'
+  text = format_text(request.body.string)
+  $ignore_text << text
 end
